21622f9c9def290936c7c5936736db63fa7f672a
wainman <swainman@fullsail.com>
2021-04-11 12:36:17 -0400

    added console log

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!2");
+            Console.WriteLine("gfdg");
+
         }
     }
 }
